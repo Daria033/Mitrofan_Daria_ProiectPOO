2954b6e90003e4f263ac23e22faa428d1f0b316a
Daria033 <148321077+Daria033@users.noreply.github.com>
2023-12-15 00:17:16 -0800

    proiect_faza3.cpp

--- proiect_faza1.cpp
+++ proiect_faza1.cpp
@@ -22,10 +22,10 @@ public:
 		this->nume = "de Urgenta 'Sfantul Constantin'";
 		this->nrMedici = 90;
 		this->nrSpecializari = 3;
-		if (this->numeSpecializari != NULL)
+		/*if (this->numeSpecializari != NULL)
 		{
 			delete[]this->numeSpecializari;
-		}
+		}*/
 		this->numeSpecializari = new string[nrSpecializari];
 		for (int i = 0; i < nrSpecializari; i++)
 		{
@@ -62,20 +62,27 @@ public:
 		}
 	}
 
-	Spital(const Spital& s): adresa(adresa){
+	//Constructor de copiere
+	Spital(const Spital& s): adresa(s.adresa){
 		this->nume = s.nume;
 		this->nrMedici = s.nrMedici;
 		this->nrSpecializari = s.nrSpecializari;
+
 		if (this->numeSpecializari != NULL) {
 			delete[]this->numeSpecializari;
 		}
-		this->numeSpecializari = new string[nrSpecializari];
-		for (int i = 0; i < nrSpecializari; i++) {
-			this->numeSpecializari[i] = s.numeSpecializari[i];
+		if (this->nrSpecializari > 0) {
+			this->numeSpecializari = new string[nrSpecializari];
+			for (int i = 0; i < nrSpecializari; i++) {
+				this->numeSpecializari[i] = s.numeSpecializari[i];
+			}
 		}
+		else this->numeSpecializari = NULL;
+		
 		this->areDepartamente = s.areDepartamente;
 	}
 
+	//Getteri
 	const string getAdresa() {
 		return this->adresa;
 	}
@@ -98,7 +105,7 @@ public:
 		return Spital::anInfiintare;
 	}
 
-
+	//Setteri
 	void setNume(string nume) {
 		this->nume = nume;
 	}
@@ -121,7 +128,7 @@ public:
 		Spital::anInfiintare = anInfiintare;
 	}
 
-
+	//Functii prietene
 	friend istream& operator>>(istream& in, Spital& spital);
 
 	friend ostream& operator<<(ostream& out, Spital& spital);
@@ -146,9 +153,90 @@ public:
 		cout << endl;
 	}
 
+	//Operatorul =
+	//evitarea auto-asignarii
+	Spital& operator=(const Spital& s) {
+		if (this != &s) {
+			this->nume = s.nume;
+			this->nrMedici = s.nrMedici;
+			this->nrSpecializari = s.nrSpecializari;
+			
+			if (this->numeSpecializari != NULL) {
+				delete[]this->numeSpecializari;
+			}
+			if (this->nrSpecializari > 0) {
+				this->numeSpecializari = new string[nrSpecializari];
+			    for (int i = 0; i < nrSpecializari; i++) {
+				     this->numeSpecializari[i] = s.numeSpecializari[i];
+			    }
+			}
+			else this->numeSpecializari = NULL;
+			
+			this->areDepartamente = s.areDepartamente;
+		}
+		return *this;
+	}
 
-}; int Spital::anInfiintare = 1923;
 
+	//Operator de indexare
+	Spital operator++() {
+		this->nrMedici;
+		return *this;
+	}
+
+	Spital operator++(int) {
+		Spital aux = *this;
+		this->nrMedici++;
+		return aux;
+	}
+
+
+
+
+	//Operator +=
+	Spital& operator+=(string nume) {
+		string* numeSpecializari = new string[nrSpecializari + 1];
+		for (int i = 0;i < nrSpecializari;i++) {
+			numeSpecializari[i] = this->numeSpecializari[i];
+		}
+		numeSpecializari[nrSpecializari] = nume;
+		nrSpecializari++;
+		if (this->numeSpecializari != NULL) {
+			delete[] this->numeSpecializari;
+		}
+		this->numeSpecializari = numeSpecializari;
+		return *this;
+
+	}
+
+	//Activitate& operator +=(Activitate a) {
+	//	this->durata += a.durata;
+	//	return *this;
+	//}
+
+	Spital& operator+=(Spital s) {
+		this->nrMedici += s.nrMedici;
+		return *this;
+	}
+
+	//Operatorul cast
+	operator int() {
+		return this->nrMedici;
+	}
+
+
+	//Operatorul index
+	string& operator[](int index) {
+		if (index >= 0 && index < this->nrSpecializari) {
+			return this->numeSpecializari[index];
+		}
+	}
+
+
+}; 
+int Spital::anInfiintare = 1923;
+
+//Operatorul >>
 istream& operator>>(istream& in, Spital& spital) {
 	cout << "Spitalul "; in >> spital.nume; 
 	//cout << " Orasul in care se afla spitalul este: "; in >> spital.adresa;
@@ -167,8 +255,9 @@ istream& operator>>(istream& in, Spital& spital) {
 	return in;
 }
 
+
 ostream& operator<<(ostream& out, Spital& spital) {
-	out << "Spitalul " << spital.nume << " are " << spital.nrMedici << " de medici si " << spital.nrSpecializari << " specializari. Acestea sunt: " << spital.numeSpecializari;
+	out << "Spitalul " << spital.nume << " are " << spital.nrMedici << " de medici si " << spital.nrSpecializari << " specializari. Acestea sunt: ";
 	if (spital.nrSpecializari != NULL) {
 		for (int i = 0;i < spital.nrSpecializari;i++) {
 			out << spital.numeSpecializari[i] << ", ";
@@ -181,19 +270,7 @@ ostream& operator<<(ostream& out, Spital& spital) {
 	return out;
 }
 
-//
-//out << "Orasul " << oras.nume << " are suprafata de " << oras.suprafata << " si populatia de " << oras.populatie << ". Varste locuitori: " << oras.varsteLocuitori;
-//if (oras.populatie != NULL) {
-//	for (int i = 0;i < oras.populatie;i++) {
-//		out << oras.varsteLocuitori[i] << ", ";
-//	}
-//}
-//else {
-//	out << "-";
-//}
-//out << endl;
-//return out;
-//}
+
 
 
 
@@ -239,6 +316,7 @@ public:
 		}
 	}
 
+	//Constructor de copiere
 	Pacient(const Pacient& p) :id(p.id) {
 		this->nume = p.nume;
 		this->nrPacienti = p.nrPacienti;
@@ -256,6 +334,7 @@ public:
 
 	}
 
+	//Getteri
 	const int getId() {
 		return this->id;
 	}
@@ -272,6 +351,7 @@ public:
 		return Pacient::gradSeveritate;
 	}
 
+	//Setteri
 	void setNume(string nume) {
 		this->nume = nume;
 	}
@@ -312,10 +392,88 @@ public:
 		cout << endl;
 	}
 
+	//Operator =
+	Pacient& operator=(const Pacient& p) {
+		if (this != &p) {
+			this->nume = p.nume;
+			this->nrPacienti = p.nrPacienti;
+			if (nrPacienti != 0) {
+				this->varstePacienti = new int[nrPacienti];
+				for (int i = 0;i < nrPacienti;i++)
+				{
+					this->varstePacienti[i] = p.varstePacienti[i];
+				}
+			}
+			
+		}
+		else {
+			this->varstePacienti = NULL;
+	    }
+		return *this;
+	}
+
+	friend ostream& operator<<(ostream& out, Pacient& pacient);
+
+	//Operator --
+
+	Pacient& operator--() {
+		if (this->nrPacienti > 0) {
+			this->nrPacienti = this->nrPacienti - 1;
+		}
+		return *this;
+	}
+	Pacient& operator--(int n) {
+		Pacient aux = *this;
+		if (this->nrPacienti > 0) {
+			this->nrPacienti = this->nrPacienti - 1;
+		}
+		return aux;
+	}
+
+	//OPerator cast
+	operator int() {
+		return this->nrPacienti;
+	}
+
+	//Operator+=
+	Pacient& operator+=(const Pacient& p) {
+		int* varsteNoi = new int[this->nrPacienti + p.nrPacienti];
+
+		// Copiem varstele existente
+		for (int i = 0; i < this->nrPacienti; ++i) {
+			varsteNoi[i] = this->varstePacienti[i];
+		}
+
+		// Adăugăm varstele pacientului p
+		for (int i = 0; i < p.nrPacienti; ++i) {
+			varsteNoi[this->nrPacienti + i] = p.varstePacienti[i];
+		}
+
+		delete[] this->varstePacienti;  // Eliberăm memoria veche
+		this->varstePacienti = varsteNoi;
+		this->nrPacienti += p.nrPacienti;
+
+		return *this;
+	}
+
+
+
 };
 int Pacient::gradSeveritate = 4;
 
+ostream& operator<<(ostream& out, Pacient& pacient) {
+	out << "Pacientii cu numele: " << pacient.nume <<endl<< "Nr. de pacienti este de: " << pacient.nrPacienti << endl << "Varstele pacientilor sunt: ";
+	if (pacient.nrPacienti != NULL) {
+		for (int i = 0;i < pacient.nrPacienti;i++) {
+			out << pacient.varstePacienti[i] << ", ";
+		}
+	}
+	else {
+		out << " - ";
+	}
 
+	return out;
+}
 
 
 
@@ -365,7 +523,8 @@ public:
 		}
 	}
 
-	Medicamente(const Medicamente& m) :id(id) {
+	//Constructor de copiere
+	Medicamente(const Medicamente& m) :id(m.id) {
 		this->nume = m.nume;
 		this->pret = m.pret;
 		this->nrSubstante = m.nrSubstante;
@@ -382,6 +541,7 @@ public:
 
 	}
 
+	//Getteri
 	const int getId() {
 		return this->id;
 	}
@@ -401,7 +561,7 @@ public:
 		return Medicamente::nrMaximSubstante;
 	}
 
-
+	//Setteri
 	void setNume(string nume) {
 		this->nume = nume;
 	}
@@ -444,12 +604,67 @@ public:
 		cout << endl;
 	}
 
+	//Operator =
+	Medicamente& operator=(const Medicamente& m) {
+		if (this != &m) {
+			this->nume = m.nume;
+			this->pret = m.pret;
+			this->nrSubstante = m.nrSubstante;
+			this->substanteActive = m.substanteActive;
+			if (nrSubstante != 0) {
+				this->substanteActive = new string[nrSubstante];
+				for (int i = 0; i < nrSubstante; i++) {
+					this->substanteActive[i] = m.substanteActive[i];
+				}
+			}
+
+		}
+		else {
+			this->substanteActive = NULL;
+		}
+		return *this;
+	}
+
+	
+	
+	//Operator +=
+	Medicamente& operator+=(const Medicamente& m) {
+		this->pret += m.pret;
+		return *this;
+	}
+
+	//Operator ==
+	bool operator==(const Medicamente& m) const {
+		if (this->id != m.id || this->nume != m.nume || this->pret != m.pret || this->nrSubstante != m.nrSubstante) {
+			return false;
+		}
+
+		// Comparam elementele din vectorul substanteActive
+		for (int i = 0; i < this->nrSubstante; ++i) {
+			if (this->substanteActive[i] != m.substanteActive[i]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	//Operator de scădere -
+	Medicamente operator-(const Medicamente& m) const {
+		Medicamente diferenta = *this;
+		diferenta.pret -= m.pret;
+		return diferenta;
+	}
+
+
+
+
 };
 int Medicamente::nrMaximSubstante = 5;
 
 
 
-void main()
+int main()
 {   //Spital
 	cout << "Spitale:" << endl;
 	string nume[] = { " Chirurgie cardiovasculara ", "Cardiologie","Pneumologie", "Endocrinologie" };
@@ -467,6 +682,7 @@ void main()
 	spital4.afisare();
 	cout << endl;
 
+	//Getteri
 	Spital spital5;
 	cout << spital5.getAdresa() << endl;
 	cout << "Spitalul " << spital5.getNume() << endl;
@@ -480,6 +696,7 @@ void main()
 	cout << "Anul de infiintare este: " << spital5.getInfiintare() << endl;
 	cout << endl;
 
+	//Setteri
 	string numeNoi[] = { "Cardiologie", "Ortopedie" };
 	spital5.setNume("Spitalul Clinic Municipal de Urgenta");
 	spital5.setNrMedici(67);
@@ -497,15 +714,58 @@ void main()
 	cout << endl;
 	cout << endl;
 
+	//Functie globala
+	
 	//Spital spital6;
-	cin >> spital2;
-	cout << spital2;
+	//cin >> spital2;
+	//cout << spital2;
+	cout << endl;
+	cout << endl;
+
+	cout << "Operatorii: = , ++, cast, index." << endl;
+
+	//operator =
+	Spital spital6;
+	spital6 = spital2;
+	cout << spital6 << endl;
+	cout << endl;
+	cout << endl;
+
+	//Operator de indexare
+	cout << "Postincrementare: " << endl;
+	Spital spital7;
+	spital7 = spital2++;
+	cout << spital2 << endl;
+	cout << spital7 << endl;
+	cout << endl;
+
+	cout << "Preincrementare: " << endl;
+	Spital spital8;
+	spital8 = ++spital2;
+	cout << spital2 << endl;
+	cout << spital8 << endl;
 	cout << endl;
 	cout << endl;
 
 
 
-    //Pacienti
+	//operator cast
+	cout << "Numarul de medici din Spitalul de Urgenta 'Sfantul Constantin' este:" << endl;
+	int b = spital1;
+	cout << b << endl;
+	cout << endl;
+	cout << endl;
+
+	//operator index
+	cout << spital1[1] << endl;
+	spital1[0] = "Chirurgie generala";
+	cout << endl;
+	cout << endl;
+
+
+
+
+    //PACIENTI
 	cout << "Pacienti:" << endl;
 	int varste[] = {14,21,32};
 	Pacient pacient1;
@@ -515,10 +775,12 @@ void main()
 	Pacient pacient3(7, "Andrei");
 	pacient3.afisare();
 	cout << endl;
+	//Constructor de copiere
 	Pacient pacient4 = pacient1;
 	pacient4.afisare();
 	cout << endl;
 
+	//Getteri
 	Pacient pacient5;
 	cout << pacient5.getId() << endl;
 	cout << "Nume: " << pacient5.getNume() << endl;
@@ -531,6 +793,7 @@ void main()
 	cout << "Grad severitate: " << pacient5.getGrad() << endl;
 	cout << endl;
 
+	//Setteri
 	int varsteNoi[] = { 22,41 };
 	pacient5.setNume("Ana");
 	pacient5.setPacienti(2, varsteNoi);
@@ -544,9 +807,62 @@ void main()
 	cout << endl;
 	cout << "Grad severitate: " << pacient5.getGrad() << endl;
 	cout << endl;
+	cout << endl;
+	
+	cout << "Operatorii: = , --, cast, +=." << endl;
+
+	//Operator =
+	Pacient pacient6;
+	pacient6 = pacient2;
+	cout << pacient6.getId() << endl;
+	cout << "Nume: " << pacient6.getNume() << endl;
+	cout << "Nr. pacienti: " << pacient6.getNrPacienti() << endl;
+	cout << "Varstele pacientilor: ";
+	for (int i = 0;i < pacient6.getNrPacienti();i++) {
+		cout << pacient6.getVarstePacienti()[i] << ", ";
+	}
+	cout << endl;
+	cout << "Grad severitate: " << pacient6.getGrad() << endl;
+	cout << endl;
 
 
-	//Medicamente
+	cout << endl;
+	cout << endl;
+
+	cout << pacient2 << endl;
+	cout << endl;
+	//Operator --
+	Pacient pacient7;
+	--pacient2;
+	pacient7 = pacient2--;
+	cout << pacient2;
+	cout << endl;
+	cout << endl;
+	cout << pacient7;
+	cout << endl;
+	cout << endl;
+	
+	//operator cast
+	cout << "Numarul de pacienti cu numele de Ana este:" << endl;
+	int p = pacient5;
+	cout << p << endl;
+	cout << endl;
+	cout << endl;
+
+	cout << endl;
+	// Utilizare operator +=
+	pacient1 += pacient5;
+
+	cout << "\nPacient 1 dupa adaugare:" << endl;
+	pacient1.afisare();
+	cout << endl;
+	cout << endl;
+
+
+	cout << endl;
+
+
+	//MEDICAMENTE
 	cout << "Medicamente:" << endl;
 	string substante[] = {"Amoxicilina","Acid clavulanic"};
 	Medicamente medicament1;
@@ -563,6 +879,7 @@ void main()
 	cout << endl;
 	cout << endl;
 
+	//Getteri
 	Medicamente med5;
 	cout << med5.getId() << endl;
 	cout << "Nume medicament: " << med5.getNume() << endl;
@@ -576,6 +893,7 @@ void main()
 	cout << "Nr. maxim de substante active: " << med5.getMaxSubstante() << endl;
 	cout << endl;
 	
+	//Setteri
 	string substanteNoi[] = { "Clorhidratul de drotaverina" };
 	med5.setNume("No-Spa");
 	med5.setPret(15.98);
@@ -587,4 +905,50 @@ void main()
 	for (int i = 0;i < med5.getNrSubstante();i++) {
 		cout << med5.getSubstanteActive()[i] << endl;
 	}
+	cout << endl;
+	cout << endl;
+
+
+	cout <<"Operatorii: = , +=, ==, de scadere."<< endl;
+
+	//operator =
+	Medicamente med6;
+	med6 = medicament2;
+	cout << med6.getId() << endl;
+	cout << "Nume medicament: " << med6.getNume() << endl;
+	cout << "Pret: " << med6.getPret() << endl;
+	cout << "Nr substante active: " << med6.getNrSubstante() << endl;
+	cout << "Substante active: ";
+	for (int i = 0;i < med6.getNrSubstante();i++) {
+		cout << med6.getSubstanteActive()[i] << ", ";
+	}
+	cout << endl;
+	cout << "Nr. maxim de substante active: " << med6.getMaxSubstante() << endl;
+	cout << endl;
+	cout << endl;
+
+
+	//Operator +=
+	medicament2 += medicament3;
+
+	cout << "Pretul dupa adunare: " << medicament2.getPret() << endl;
+
+	cout << endl;
+	cout << endl;
+	//Operator ==
+	if (medicament1 == medicament2) {
+		cout << "Medicamentele sunt identice." << endl;
+	}
+	else {
+		cout << "Medicamentele sunt diferite." << endl;
+	}
+
+
+	cout << endl;
+	cout << endl;
+
+	//Operator de scădere -
+	Medicamente medicamentDiferenta = medicament1 - medicament2;
+	cout << "Pretul diferentei: " << medicamentDiferenta.getPret() << endl;
+
 }
